08b680f8b1236747dc0e997df061f69fbe540ee1
AbdurRob <abdurrobr34l@gmail.com>
2026-01-07 23:35:31 +0600

    Completed first probelm

---
+++ Problem-1.js
@@ -0,0 +1,13 @@
+//* PROBLEM 1: REVERSE A STRING
+
+function reverseString(string) {
+  let reversedString = "";
+
+  for (let i= string.length - 1; i>=0; i--) {
+    reversedString += string[i];
+  }
+
+  return reversedString;
+}
+
+console.log(reverseString("hello"));
