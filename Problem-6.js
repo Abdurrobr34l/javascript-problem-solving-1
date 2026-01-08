2f582b4f84c007434da3f9721f1d7cd2a12a01a0
AbdurRob <abdurrobr34l@gmail.com>
2026-01-08 15:01:41 +0600

    Solved problem 6

--- Problem-6.js
+++ Problem-6.js
@@ -4,3 +4,15 @@
 // Example:
 // Input: [1, 2, 3, 4]
 // Output: 10
+
+function sumOfNumber(numberArray) {
+  let totalSum = 0;
+
+  for (let i = 0; i < numberArray.length; i++) {
+    totalSum += numberArray[i];
+  };
+
+  return totalSum;
+}
+
+console.log(sumOfNumber([1, 2, 3, 4]));
--- script.js
+++
@@ -1,6 +0,0 @@
-//* PROBLEM 6: SUM OF ALL NUMBERS IN AN ARRAY
-// Write a function that returns the sum of all numbers in an array.
-
-// Example:
-// Input: [1, 2, 3, 4]
-// Output: 10
